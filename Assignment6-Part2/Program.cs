3b8baf6d682e41d7d6c478d28c7e932931f40f9a
Sarah <sarahelhefnawy0@gmail.com>
2026-02-16 00:37:14 +0200

    Add project files.

---
+++ Assignment6-Part2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment6_Part2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Assignment6/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment6_Part1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
